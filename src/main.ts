84920c4de6d53226c154a10bd673cf1600856a24
sisa1313 <anyu1234@126.com>
2021-08-18 15:31:13 +0800

    vConsole

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import store from './store';
 import tools from './utils/tools.js';
 import axios from './utils/axios.js';
 import './utils/rem';
+import VConsole from 'vconsole';
 
 const app = createApp(App);
 app.use(router);
@@ -14,3 +15,8 @@ app.mount('#app');
 // 挂载全局属性和方法
 app.config.globalProperties.$tools = tools;
 app.config.globalProperties.$axios = axios;
+
+// 开发环境开启vConsole
+if (import.meta.env.MODE) {
+  const vConsole = new VConsole();
+}
